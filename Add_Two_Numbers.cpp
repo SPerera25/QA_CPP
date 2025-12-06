b18fef6abc7187132ea3d4e343c9beb5e46804c4
S Perera <sperera1922@gmail.com>
2025-12-06 23:33:13 +0530

    Add two user-input numbers

---
+++ Add_Two_Numbers.cpp
@@ -0,0 +1,17 @@
+//Get two numbers from the user and add them together
+#include <iostream>
+using namespace std;
+int main() {
+
+    double num1, num2, sum;
+
+    cout << "Enter your first number: ";
+    cin >> num1;
+    cout << "Enter tour second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+    return 0;
+}
