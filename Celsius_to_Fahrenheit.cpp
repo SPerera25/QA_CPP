f5d459fe079d969ef7a20ab64fb1df5b91bd20db
S Perera <sperera1922@gmail.com>
2025-12-06 23:18:50 +0530

    Add Celsius to Fahrenheit conversion program

---
+++ Celsius_to_Fahrenheit.cpp
@@ -0,0 +1,11 @@
+//Convert the given temperature in Celsius to Fahrenheit
+#include <iostream>
+using namespace std;
+int main() {
+    double celsius, fahrenheit;
+    cout << "Enter temperature in celsius: ";
+    cin >> celsius;
+    fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+    cout << "Temperature in fahrenheit: " << fahrenheit << endl;
+    return 0;
+}
