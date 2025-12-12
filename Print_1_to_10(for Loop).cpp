a6495ee007896cb1b6b841b5ffd7cc597a25e686
S Perera <sperera1922@gmail.com>
2025-12-12 17:32:32 +0530

    Print numbers from 1 to 10 using a for loop.

---
+++ Print_1_to_10(for Loop).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    for (int i = 1; i <= 10; i++) {
+        cout << i << endl;
+    }
+    // i++ = i + 1 (post increment)
+    // i value increases by 1 after each loop iteration
+    
+    return 0;
+}
