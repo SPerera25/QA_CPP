a4e0dcffd2a2591a95109ce454fec9c3a7a11ce4
S Perera <sperera1922@gmail.com>
2025-12-12 14:48:17 +0530

    Fix salary raise calculation to use decimal division
    Updated salary calculation to use decimal for percentage.

--- Calculate_Salary.cpp
+++ Calculate_Salary.cpp
@@ -8,7 +8,11 @@ then a $1500 deduction for taxes. Print the final salary.*/
 using namespace std;
 int main () {
     int salary = 50000;
-    salary += salary * (10/100); // 10% raise
+    salary += salary * (10.0/100); // 10% raise
+    /*In that percentage, we have to make one or the both numbers decimal.
+    Because dividing decimal numbers give a decimal number.
+    Or
+    we can put 0.10 there.*/
     salary += 2000;          // $2000 bonus
     salary -= 1500;          // $1500 deduction for taxes
     cout << "The final salary is $" << salary << endl;
