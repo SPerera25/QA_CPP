205aa418ef444e9abc2be59061b9c5292a8090b0
S Perera <sperera1922@gmail.com>
2025-12-12 21:01:36 +0530

    Modifying the code

--- Area_and_Perimeter_(Rectangle).cpp
+++ Area_and_Perimeter_(Rectangle).cpp
@@ -6,15 +6,15 @@ int main () {
 
     double length, width, area, perimeter;
 
-    cout<<"Enter the length of the rectangle: ";
+    cout <<"Enter the length of the rectangle: ";
     cin >> length;
-    cout<<"Enter the width of the rectangle";
+    cout <<"Enter the width of the rectangle";
     cin >> width;
 
-    area = length + width;
+    area = length * width;
     perimeter = 2 * (length + width);
 
-    cout<<"The area of the rectangle is "<<area<<endl;
-    cout<<"The perimeter of the rectangle is "<<perimeter<<endl;
+    cout <<"The area of the rectangle is "<< area <<endl;
+    cout <<"The perimeter of the rectangle is "<< perimeter <<endl;
     return 0;
 }
