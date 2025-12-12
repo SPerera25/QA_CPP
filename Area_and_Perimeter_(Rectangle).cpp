a8839fa04ba31a72487e85bd941f79b76a23c380
S Perera <sperera1922@gmail.com>
2025-12-12 14:03:32 +0530

    Add program to calculate area and perimeter of a rectangle

---
+++ Area_and_Perimeter_(Rectangle).cpp
@@ -0,0 +1,19 @@
+/*Find the area and perimeter of a rectangle*/
+#include <iostream>
+using namespace std;
+int main () {
+
+    double length, width, area, perimeter;
+
+    cout<<"Enter the length of the rectangle: ";
+    cin >> length;
+    cout<<"Enter the width of the rectangle";
+    cin >> width;
+
+    area = length + width;
+    perimeter = 2 * (length + width);
+
+    cout<<"The area of the rectangle is "<<area<<endl;
+    cout<<"The perimeter of the rectangle is "<<perimeter<<endl;
+    return 0;
+}
