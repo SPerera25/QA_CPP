4b25b98c88cfdfd7c0c78f380a6033a740f8074e
S Perera <sperera1922@gmail.com>
2025-12-19 15:15:53 +0530

    Functions question no 2

---
+++ Functions(Q2).cpp
@@ -0,0 +1,36 @@
+/* Write a program using functions to perform addition, subtraction,
+multiplication, and division on two numbers. Implement each operation 
+as a separate function. */
+
+#include <iostream>
+using namespace std;
+
+double add (double x, double y) {
+    return x + y;
+}
+
+double substract (double x, double y){
+    return x - y;
+}
+
+double multiply (double x, double y){
+    return x * y;
+}
+
+double divide (double x, double y){
+    return x / y;
+}
+
+int main (){
+    double num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Addition: " << add (num1, num2) << endl;
+    cout << "Subtraction: " << substract (num1, num2) << endl;
+    cout << "Multiplication: " << multiply (num1, num2) << endl;
+    cout << "Division: " << divide (num1, num2) << endl;
+
+    return 0;
+}
