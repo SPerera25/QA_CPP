19d4ad14f93b2db7eff3e4f8ca2c887c0ca90ddf
S Perera <sperera1922@gmail.com>
2025-12-19 20:35:22 +0530

    Find whether the number is prime or not

---
+++ Functions(Q3).cpp
@@ -0,0 +1,32 @@
+/* Write a function named isPrime(int n) that returns true if the number is
+prime and false otherwise. Use this function in main(). */
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+
+    if ( n <= 1 )
+        return false;
+
+    for ( int i = 2; i * i <= n; i++ ) {
+        if ( n % i == 0 )
+            return false;
+    }
+
+    return true;
+}
+
+int main () {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if ( isPrime(number) ){
+        cout << number << " is a prime number." << endl;
+    }
+    else {
+        cout << number << " is not a prime number." << endl;
+    }
+}
