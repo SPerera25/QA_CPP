551ad64189a1db28eafffaa88c96c51bbab63e73
S Perera <sperera1922@gmail.com>
2026-01-05 17:12:56 +0530

    Check the eligibilty to drive

--- Check_eligibility_to_Drive.cpp
+++ Check_eligibility_to_Drive.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
 int main (){
-    
+    int age;
+	cout << "Enter your age in years: ";
+	cin >> age;
+	if ( age <= 0 || age >= 120 ){
+		cout << "Invalid age.";
+	}
+	else if ( age >= 18 ){
+		cout << "You are eligible to drive.";
+	}
+	else if ( age < 18 ){
+		cout << "You are not eligible to drive." << endl;
+		cout << "You have to wait " << 18 - age << " Years to drive." << endl;
+	}
+	return 0;
 }
